0f61185fccec79bf9db6ee7f852e960a1222590f
Davon <dkwebb95@gmail.com>
2022-05-16 22:20:45 -0400

    added integer values for shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+
 var fightOrSkip = function() {
     // Ask player if they want to skip or fight a battle
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -119,19 +120,18 @@ var endGame = function() {
 var shop = function() {
     // Ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', 'LEAVE' to make a choice"
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, 3 for LEAVE."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch(shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;           
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
+            window.alert("Leaving the store.")
             // Do nothing, so function will end
             break;
         default:
